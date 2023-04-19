eac2a67306cdca32fd39f86ece47e62f45766316
elijahchi <zbchi1986@gmail.com>
2023-04-18 22:08:01 -0400

    todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import react from 'react';
 
 function App() {
+
+  const [todo, setTodo] = useState();
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    const todo = e.target.value;
+    setTodo(todo)
+  }
+
+  const handleClick = () => {
+    if (!todo) {
+      return;
+    }
+    const newTodos = todos.concat({ name: todo, done: false });
+    setTodos(newTodos);
+    setTodo('')
+  }
+
+  const handleToggle = (index) => {
+    const newTodos = todos.map((todo, i) => {
+      if (i !== index) {
+        return todo;
+      }
+      return { name: todo.name, done: !todo.done };
+    })
+    setTodos(newTodos);
+  }
+
+  const remainingTodos = todos.filter(todo => !todo.done).length;
+
+  const handleDelete = (todo) => {
+    const newTodos = todos.filter(t => t !== todo);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className='remaining'>RemainingTodos: {remainingTodos}</div>
+      <div>
+        <input type='text' onChange={handleChange} value={todo}></input>
+        <button onClick={handleClick}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, i) => {
+          return (
+            <li className='item'>
+              <span
+              className={todo.done ? "done": ""}><input
+                className='checkbox'
+                type="checkbox"
+                checked={todo.done ? 'checked' : ''}
+                onClick={() => handleToggle(i)}
+              ></input>{todo.name}</span><button onClick={() => handleDelete(todo)}>Delete</button></li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
